f1ab0df646831349a5e3e5bb60b74962bfe37d65
OTSvetkova <OTSvetkova@inno.tech>
2026-01-09 17:21:39 +0300

    операции(сложение, вычитание, умножение, деление) с двумя целыми числами

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,26 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Введите текст нажмите <Enter>:");
+        System.out.println("Введите первое число:");
+        int numberOne = new Scanner (System.in).nextInt();
+
+        System.out.println("Введите второе число:");
+        int numberTwo = new Scanner (System.in).nextInt();
+        int resultInt;
+        //сумма
+        resultInt = numberOne + numberTwo;
+        System.out.println("Сумма чисел : " + resultInt);
+        //разность
+        resultInt = numberOne - numberTwo;
+        System.out.println("Разность чисел :" + resultInt);
+
+        //умножение
+        resultInt = numberOne * numberTwo;
+        System.out.println("Произведение чисел :" + resultInt);
+
+        //деление
+        double resultDouble;
+        resultDouble = (double) numberOne / numberTwo;
+        System.out.println("Частное чисел :" + resultDouble);
         }
     }
